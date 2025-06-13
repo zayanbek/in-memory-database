cd64dcc9f8e5800c8645bd73db28c83828b6626c
Zayan <zayan_ahmad@hotmail.com>
2025-06-13 16:29:01 -0500

    rename hashmap to data and add getValues method

--- src/main/java/Row.java
+++ src/main/java/Row.java
@@ -2,16 +2,20 @@ import java.util.HashMap;
 
 public class Row {
 
-    private HashMap<String, Object> hashmap = new HashMap<>();
+    private HashMap<String, Object> data = new HashMap<>();
 
     public Row() {}
 
     public void setValue(String columnName, Object value) {
-        hashmap.put(columnName, value);
+        this.data.put(columnName, value);
     }
 
     public Object getValue(String columnName) {
-        return hashmap.get(columnName);
+        return this.data.get(columnName);
+    }
+
+    public HashMap<String, Object> getValues() {
+        return this.data;
     }
 
 }
